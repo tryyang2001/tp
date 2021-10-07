048bd2a414adecb6d34cfbb424e9e455b4970b14
Tay Weida <weida.tay99@gmail.com>
2021-10-08 02:54:35 +0800

    Update JUnit test to pass gradle

--- src/test/java/seedu/duke/profile/ProfileTest.java
+++ src/test/java/seedu/duke/profile/ProfileTest.java
@@ -2,12 +2,12 @@ package seedu.duke.profile;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProfileTest {
 
     @Test
-    void calculateBMI_twoIntInputs_expectDoubleReturned() {
+    void calculateBmi_twoIntInputs_expectDoubleReturned() {
         assertEquals(20.8, Profile.calculateBmi(170, 60));
     }
 }
