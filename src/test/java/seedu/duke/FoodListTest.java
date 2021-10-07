d595e06c51a4bd1d67efdf5f5abbe1ff16b1bd91
e0550472@u.nus.edu <tryyang2001@gmail.com>
2021-10-07 22:17:28 +0800

    JUnit Test for FoodList

---
+++ src/test/java/seedu/duke/FoodListTest.java
@@ -0,0 +1,53 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FoodListTest {
+
+    @Test
+    void testAddFoodUsingFoodClassParameter_foodClassParameter_expectExistsInList() {
+        FoodList foodlist = new FoodList();
+        foodlist.add(new Food("chicken rice", 607));
+        assertEquals("chicken rice (607 cal)", foodlist.get(foodlist.size() - 1).toString());
+    }
+
+    @Test
+    void testAddFoodUsingStringParameters_stringParameterInputs_expectExistsInList() {
+        FoodList foodlist = new FoodList();
+        foodlist.add("chicken rice", 607);
+        assertEquals("chicken rice (607 cal)", foodlist.get(foodlist.size() - 1).toString());
+    }
+
+    @Test
+    void printEmptyFoodList_noItemInList_expectErrorMessage() {
+        new FoodList().printFoodList();
+    }
+
+    @Test
+    void deleteExistingFoodItem_validIndexInput_expectDeleteSuccessful() {
+        FoodList foodList = new FoodList();
+        foodList.add("chicken rice", 607);
+        foodList.add("yong tau foo", 536);
+        foodList.delete(1);
+        assertNotEquals(2, foodList.size());
+    }
+
+    @Test
+    void deleteNonExistingFoodItem_invalidIndexInput_expectIndexOutOfBoundException() {
+        FoodList foodList = new FoodList();
+        assertThrows(IndexOutOfBoundsException.class, () -> foodList.delete(0));
+    }
+
+    @Test
+    void deleteAllFoodItems_callDeleteAllMethod_expectEmptyList() {
+        FoodList foodList = new FoodList();
+        foodList.add("chicken rice", 607);
+        foodList.add("yong tau foo", 536);
+        foodList.add("mcspicy alacarte", 528);
+        foodList.add("char kway teow", 744);
+        foodList.deleteAll();
+        assertEquals(0, foodList.size());
+    }
+}
