03105d0accc856b0f97ce72d994924eedb81bdf6
Tay Weida <weida.tay99@gmail.com>
2021-10-09 01:44:50 +0800

    Update exceptions to be thrown and add JUnit tests for exception

--- src/main/java/seedu/duke/profile/Profile.java
+++ src/main/java/seedu/duke/profile/Profile.java
@@ -1,27 +1,34 @@
 package seedu.duke.profile;
 
+import seedu.duke.profile.exceptions.InvalidCharacteristicException;
+
+/**
+ * Profile that contains the relevant data input by user.
+ */
 public class Profile {
-    private int height;
-    private int weight;
+    private double height;
+    private double weight;
     private int calorieGoal;
 
-    public Profile(int height, int weight) {
+    public Profile(double height, double weight) throws InvalidCharacteristicException {
         setHeight(height);
         setWeight(weight);
         setCalorieGoal(0); //Initialize to 0 first
     }
 
-    public Profile() {
+    public Profile() throws InvalidCharacteristicException {
         setHeight(0);
         setWeight(0);
         setCalorieGoal(0);
     }
 
-    public void setHeight(int height) {
+    public void setHeight(double height) throws InvalidCharacteristicException {
+        checkHeightValidity(height);
         this.height = height;
     }
 
-    public void setWeight(int weight) {
+    public void setWeight(double weight) throws InvalidCharacteristicException {
+        checkWeightValidity(weight);
         this.weight = weight;
     }
 
@@ -30,15 +37,40 @@ public class Profile {
     }
 
     public double calculateBmi() {
-        double heightInM = ((double) height) / 100.0;
-        double bmi = Math.round((weight / (Math.pow(heightInM, 2))) * 10) / 10.0;
-        return bmi;
+        return computeBmi(height, weight);
     }
 
-    public static double calculateBmi(int height, int weight) {
-        double heightInM = ((double) height) / 100.0;
-        double bmi = Math.round((weight / (Math.pow(heightInM, 2))) * 10) / 10.0;
-        return bmi;
+    /**
+     * Manually calculates the BMI.
+     * Used when the user decides that he input values that is not be stored in his profile.
+     *
+     * @param height Value of manual input of height
+     * @param weight Value of manual input of weight
+     * @return The calculated BMI of the manual inputs
+     * @throws InvalidCharacteristicException When the user inputs negative values for either height or weight
+     */
+    public static double calculateBmi(double height, double weight) throws InvalidCharacteristicException {
+        checkHeightValidity(height);
+        checkWeightValidity(weight);
+
+        return computeBmi(height, weight);
+    }
+
+    private static double computeBmi(double height, double weight) {
+        double heightInM = height / 100.0;
+        return Math.round((weight / (Math.pow(heightInM, 2))) * 10) / 10.0;
+    }
+
+    private static void checkWeightValidity(double weight) throws InvalidCharacteristicException {
+        if (weight < 0) {
+            throw new InvalidCharacteristicException("Weight");
+        }
+    }
+
+    private static void checkHeightValidity(double height) throws InvalidCharacteristicException {
+        if (height < 0) {
+            throw new InvalidCharacteristicException("Height");
+        }
     }
 
     public int calculateNetCalories(int foodCalories, int exerciseCalories) {
--- src/test/java/seedu/duke/profile/ProfileTest.java
+++ src/test/java/seedu/duke/profile/ProfileTest.java
@@ -1,13 +1,45 @@
 package seedu.duke.profile;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.profile.exceptions.InvalidCharacteristicException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class ProfileTest {
 
     @Test
-    void calculateBmi_twoIntInputs_expectDoubleReturned() {
-        assertEquals(20.8, Profile.calculateBmi(170, 60));
+    void Profile_negativeInput_expectException() {
+        assertThrows(InvalidCharacteristicException.class,
+                () -> {
+                    Profile p = new Profile(-1,-1);
+                });
     }
+
+    @Test
+    void setHeight_negativeInput_expectException() throws InvalidCharacteristicException {
+        Profile p = new Profile(170.2,60.5);
+        assertThrows(InvalidCharacteristicException.class,
+                () -> p.setHeight(-1));
+    }
+
+    @Test
+    void setWeight_negativeInput_expectException() throws InvalidCharacteristicException {
+        Profile p = new Profile(170.2, 60.5);
+        assertThrows(InvalidCharacteristicException.class,
+                () -> p.setWeight(-1));
+    }
+
+    @Test
+    void calculateBmi_twoDoubleInputs_expectDoubleReturned() throws InvalidCharacteristicException {
+        assertEquals(20.4, Profile.calculateBmi(171.2, 59.8));
+    }
+
+    @Test
+    void calculateBmi_negativeInputs_expectException() {
+        assertThrows(InvalidCharacteristicException.class,
+                () -> Profile.calculateBmi(-171.2, -59.8));
+    }
+
 }
