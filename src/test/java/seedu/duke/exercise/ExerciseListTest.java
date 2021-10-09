43f64d8c49ee0d1ec13bac0ecdea139841034d9e
E0543747@u.nus.edu <E0543747@u.nus.edu>
2021-10-10 00:08:17 +0800

    JUnit Test for ExerciseList

---
+++ src/test/java/seedu/duke/exercise/ExerciseListTest.java
@@ -0,0 +1,48 @@
+package seedu.duke.exercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class ExerciseListTest {
+
+    @Test
+    void addExercise_exerciseClassParameter_expectAddInList() {
+        ExerciseList exerciseList = new ExerciseList();
+        exerciseList.addExercise(new Exercise("Jumping Jacks", 100));
+        assertEquals("Jumping Jacks (100 cal)", exerciseList.getExercise(0).toString());
+    }
+
+    @Test
+    void deleteExercise_exerciseIndex_expectDeleteFromList() {
+        ExerciseList exerciseList = new ExerciseList();
+        exerciseList.addExercise(new Exercise("Running", 250));
+        exerciseList.addExercise(new Exercise("Jumping Jacks", 100));
+        exerciseList.deleteExercise(0);
+        assertEquals("Jumping Jacks (100 cal)", exerciseList.getExercise(0).toString());
+    }
+
+    @Test
+    void deleteExercise_exerciseIndex_expectCorrectNumberOfTasksLeft() {
+        ExerciseList exerciseList = new ExerciseList();
+        exerciseList.addExercise(new Exercise("Running", 250));
+        exerciseList.addExercise(new Exercise("Jumping Jacks", 100));
+        exerciseList.deleteExercise(0);
+        assertEquals( 1, exerciseList.getSize());
+    }
+
+    @Test
+    void deleteExercise_invalidIndex_expectException() {
+        ExerciseList exerciseList = new ExerciseList();
+        assertThrows(IndexOutOfBoundsException.class , () -> exerciseList.deleteExercise(0));
+    }
+
+    @Test
+    void printExerciseList_threeExercisesInList_expectPrintFromList() {
+        ExerciseList exerciseList = new ExerciseList();
+        exerciseList.addExercise(new Exercise("Running", 250));
+        exerciseList.printExerciseList();
+    }
+}
