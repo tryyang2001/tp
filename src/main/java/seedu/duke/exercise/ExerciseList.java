d800d7c43b3ab87af277ddfb612e6794c51cac84
E0543747@u.nus.edu <E0543747@u.nus.edu>
2021-10-11 14:09:43 +0800

    added totalCalorie method for ExerciseList and some tests

--- src/main/java/seedu/duke/exercise/ExerciseList.java
+++ src/main/java/seedu/duke/exercise/ExerciseList.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 
 public class ExerciseList {
 
-    public static final String MESSAGE_EXERCISE_ADDED = "An exercise has been added:";
-    private static final String MESSAGE_EXERCISE_DELETED = "You have removed the exercise:";
-    private static final String MESSAGE_EMPTY_EXERCISE_LIST = "No exercise is found.";
     private static final String LS = System.lineSeparator();
 
     private ArrayList<Exercise> exerciseList = new ArrayList<>();
@@ -46,9 +43,6 @@ public class ExerciseList {
      */
     public void addExercise(Exercise exercise) {
         this.exerciseList.add(exercise);
-
-        System.out.println(MESSAGE_EXERCISE_ADDED);
-        System.out.println("\t" + exercise);
     }
 
     /**
@@ -60,34 +54,32 @@ public class ExerciseList {
         return exerciseList.remove(index);
     }
 
-    /**
-     * Prints out all the exercises in the exercise list.
-     */
-    public void printExerciseList() {
-        if (exerciseList.size() == 0) {
-            System.out.println(MESSAGE_EMPTY_EXERCISE_LIST);
-        } else if (exerciseList.size() == 1) {
-            System.out.println("You have done 1 exercise:");
-            System.out.println("\t" + exerciseList.get(0));
-        } else {
-            System.out.println("You have done " + exerciseList.size() + " exercises:");
-            for (int i = 0; i < exerciseList.size(); i++) {
-                System.out.println("\t" + i + 1 + ". " + exerciseList.get(i));
-            }
-        }
-    }
+//    /**
+//     * Prints out all the exercises in the exercise list.
+//     */
+//    public void printExerciseList() {
+//        if (exerciseList.size() == 0) {
+//            System.out.println(MESSAGE_EMPTY_EXERCISE_LIST);
+//        } else if (exerciseList.size() == 1) {
+//            System.out.println("You have done 1 exercise:");
+//            System.out.println("\t" + exerciseList.get(0));
+//        } else {
+//            System.out.println("You have done " + exerciseList.size() + " exercises:");
+//            for (int i = 0; i < exerciseList.size(); i++) {
+//                System.out.println("\t" + i + 1 + ". " + exerciseList.get(i));
+//            }
+//        }
+//    }
 
     /**
      * Returns exercise list in a string format.
      *
      * @return Exercise list in string format.
      */
-    public String convertExerciseListToString() {
+    public String convertToString() {
         StringBuilder exerciseListToString;
 
-        if (exerciseList.size() == 0) {
-            exerciseListToString = new StringBuilder(MESSAGE_EMPTY_EXERCISE_LIST + LS);
-        } else if (exerciseList.size() == 1) {
+        if (exerciseList.size() == 1) {
             exerciseListToString = new StringBuilder("You have done 1 exercise:" + LS);
         } else {
             exerciseListToString = new StringBuilder("You have done " + exerciseList.size() + " exercises:" + LS);
@@ -98,6 +90,18 @@ public class ExerciseList {
         return exerciseListToString.toString();
     }
 
-    //todo get total calorie method
+    /**
+     * Computes the sum of calorie of all exercises in exercise list.
+     *
+     * @return Integer value of the sum of calorie of all exercises.
+     */
+    public int totalCalorie(){
+        int sumOfExerciseCalorie = 0;
+
+        for (Exercise exercise : exerciseList) {
+            sumOfExerciseCalorie += exercise.getCalorie();
+        }
+        return sumOfExerciseCalorie;
+    }
 
 }
