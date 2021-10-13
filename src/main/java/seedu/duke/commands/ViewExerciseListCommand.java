e19ce0264f4bde95a9ec67ac49da1640dcc1fb92
E0543747@u.nus.edu <E0543747@u.nus.edu>
2021-10-13 10:04:10 +0800

    added assertions and loggings, removed some unused static final lines

--- src/main/java/seedu/duke/commands/ViewExerciseListCommand.java
+++ src/main/java/seedu/duke/commands/ViewExerciseListCommand.java
@@ -2,6 +2,10 @@ package seedu.duke.commands;
 
 import seedu.duke.ui.Ui;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  * Represents the command that when executed, lists all the items in the ExerciseList.
  */
@@ -13,13 +17,17 @@ public class ViewExerciseListCommand extends Command {
             + Ui.LS + MESSAGE_COMMAND_FORMAT;
     public static final String MESSAGE_SUCCESS = "You have done %1$d exercise(s):" + Ui.LS + "%2$s";
 
+    private static Logger logger = Logger.getLogger("ViewExerciseCommand");
 
     @Override
     public CommandResult execute() {
         if (super.exerciseItems.getSize() == 0) {
+            logger.log(Level.WARNING,"Exercise list is empty");
             return new CommandResult(MESSAGE_EMPTY_EXERCISE_LIST);
         }
+        assert exerciseItems.getSize() > 0 : "Exercise list is not empty";
         final String stringOfAllItems = super.exerciseItems.convertToString();
+        logger.log(Level.INFO, "Exercise is converted to string to be viewed");
         return new CommandResult(String.format(MESSAGE_SUCCESS, super.exerciseItems.getSize(), stringOfAllItems));
     }
 }
