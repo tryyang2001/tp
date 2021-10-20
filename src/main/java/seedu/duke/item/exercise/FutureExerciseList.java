0385113eeae6d151b5e17893a236919439e04123
E0543747@u.nus.edu <E0543747@u.nus.edu>
2021-10-21 00:09:19 +0800

    edit to FutureExerciseList

--- src/main/java/seedu/duke/item/exercise/FutureExerciseList.java
+++ src/main/java/seedu/duke/item/exercise/FutureExerciseList.java
@@ -9,51 +9,6 @@ public class FutureExerciseList extends ExerciseList {
 
     private static final int ONE_WEEK = 7;
     private static final int ONE_DAY = 1;
-    /**
-     * Returns the future exercise list.
-     *
-     * @return Future exercise list.
-     */
-    public ArrayList<Exercise> getFutureExerciseList() {
-        return exerciseList;
-    }
-
-    /**
-     * Returns exercise item as a specific index in the future exercise list.
-     *
-     * @param index Index of the exercise item.
-     * @return exercise item in the future exercise list.
-     */
-    public Exercise getFutureExercise(int index) {
-        return exerciseList.get(index);
-    }
-
-    /**
-     * Adds an exercise item into the future exercise item and sort by date in an ascending order.
-     *
-     * @param exercise Exercise class object to be added.
-     */
-    public void addFutureExercise(Exercise exercise) {
-        exerciseList.add(exercise);
-        sortFutureExerciseList();
-    }
-
-    /**
-     * Deletes and exercise item from the future exercise list.
-     *
-     * @param index Index of the exercise to be deleted.
-     * @return Exercise object removed.
-     */
-    public Exercise deleteFutureExercise(int index) {
-        return exerciseList.remove(index);
-    }
-
-    /**
-     * Deletes all exercises in the future exercise list.
-     */
-    public void clearFutureExerciseList() {
-        exerciseList.clear();
-    }
 
     /**
      * Converts the entire future exercise list to string format for printing purpose.
@@ -79,40 +34,7 @@ public class FutureExerciseList extends ExerciseList {
         }
         return futureExerciseListToString.toString().stripTrailing();
     }
-
-    /**
-     * Returns the number of exercises in the future exercise list.
-     *
-     * @return Number of exercises in the future exercise list.
-     */
-    @Override
-    public int getSize() {
-        return exerciseList.size();
-    }
-
-    /**
-     * Computes the sum of calorie of all exercises in the future exercise list.
-     *
-     * @return Integer value of the sum of calorie of all exercises in the future exercise list.
-     */
-    @Override
-    public int getTotalCalories() {
-        int sumOfExerciseCalorie = 0;
-
-        for (Exercise exercise : exerciseList) {
-            sumOfExerciseCalorie += exercise.getCalories();
-        }
-        return sumOfExerciseCalorie;
-    }
-
-    /**
-     * Sorts the future exercise list in ascending format according to the date.
-     *
-     */
-    public void sortFutureExerciseList() {
-        exerciseList.sort(Comparator.comparing(Exercise::getDate));
-    }
-
+    
     /**
      * Adds all recurring exercises between two dates into the FutureExerciseList.
      */
