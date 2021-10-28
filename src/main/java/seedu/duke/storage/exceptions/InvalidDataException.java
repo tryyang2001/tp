ab9d22372d487b6272b65f62c77827387b29aa12
Tay Weida <weida.tay99@gmail.com>
2021-10-28 21:34:38 +0800

    Update Storage loading format

--- src/main/java/seedu/duke/storage/exceptions/InvalidDataException.java
+++ src/main/java/seedu/duke/storage/exceptions/InvalidDataException.java
@@ -5,8 +5,10 @@ package seedu.duke.storage.exceptions;
  */
 public class InvalidDataException extends Exception {
     private static final String LS = System.lineSeparator();
-    private static final String ERROR_MESSAGE = "There is a invalid line in %1$s." + LS
-            + "\"%2$s\" will not be loaded into the bot.";
+    private static final String DIVIDER = "...........................................................................";
+    private static final String ERROR_MESSAGE = DIVIDER + LS
+            + "There is a invalid line in %1$s." + LS
+            + "\"%2$s\" will not be loaded into the bot." + LS + DIVIDER;
 
     public InvalidDataException(String file, String inputData) {
         super(String.format(ERROR_MESSAGE, file, inputData));
