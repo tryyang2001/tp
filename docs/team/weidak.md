b930fa419bf76b603c6d00c4132267d7b3e563aa
Tay Weida <weida.tay99@gmail.com>
2021-11-05 03:27:08 +0800

    Fix storage error handling

--- docs/team/weidak.md
+++ docs/team/weidak.md
@@ -10,3 +10,15 @@ Fitbot is a app for people who are interested in tracking calories
 - Profile 
 - DG: Diagrams for Profile, Storage
 - User guide: Profile and bmi commands
+
+#### **New Feature**:
+
+#### **Code Contributed**:
+
+#### **Project Management**:
+
+#### **Enhancements To Existing Features**:
+
+#### **Documentation**:
+
+#### **Community**:
--- src/main/java/seedu/duke/storage/data/profile/ProfileDecoder.java
+++ src/main/java/seedu/duke/storage/data/profile/ProfileDecoder.java
@@ -1,9 +1,17 @@
 package seedu.duke.storage.data.profile;
 
 import seedu.duke.data.profile.Profile;
+import seedu.duke.data.profile.attributes.ActivityFactor;
+import seedu.duke.data.profile.attributes.Age;
+import seedu.duke.data.profile.attributes.CalorieGoal;
+import seedu.duke.data.profile.attributes.Gender;
+import seedu.duke.data.profile.attributes.Height;
+import seedu.duke.data.profile.attributes.Name;
+import seedu.duke.data.profile.attributes.Weight;
 import seedu.duke.data.profile.exceptions.InvalidCharacteristicException;
 import seedu.duke.storage.StorageManager;
 import seedu.duke.storage.exceptions.InvalidDataException;
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -42,18 +50,74 @@ public class ProfileDecoder {
         try {
             Profile profile = new Profile();
             final String[] profileDetails = input.split(StorageManager.FILE_TEXT_DELIMITER);
-            final String name = profileDetails[0];
-            final double height = Double.parseDouble(profileDetails[1]);
-            final double weight = Double.parseDouble(profileDetails[2]);
-            final char gender = profileDetails[3].charAt(0);
-            final int age = Integer.parseInt(profileDetails[4]);
-            final int calorieGoal = Integer.parseInt(profileDetails[5]);
-            final int activityFactor = Integer.parseInt(profileDetails[6]);
-            profile.setProfileWithRawInputs(name, height, weight, gender, age, calorieGoal, activityFactor);
+            final Name name = decodeName(profileDetails[0]);
+            final Height height = decodeHeight(profileDetails[1]);
+            final Weight weight = decodeWeight(profileDetails[2]);
+            final Gender gender = decodeGender(profileDetails[3]);
+            final Age age = decodeAge(profileDetails[4]);
+            final CalorieGoal calorieGoal = decodeCalorieGoal(profileDetails[5]);
+            final ActivityFactor activityFactor = decodeActivityFactor(profileDetails[6]);
+            profile.setProfile(name, height, weight, gender, age, calorieGoal, activityFactor);
             return profile;
         } catch (IndexOutOfBoundsException | NumberFormatException e) {
             throw new InvalidDataException(StorageManager.FILENAME_PROFILE, input);
         }
     }
 
+    private static Name decodeName(String detail) {
+        return new Name(detail);
+    }
+
+    private static Height decodeHeight(String detail) {
+        try {
+            return new Height(Double.parseDouble(detail));
+        } catch (NumberFormatException e) {
+            //Returns an invalid height for startup to detect
+            return new Height(Double.MIN_VALUE);
+        }
+    }
+
+    private static Weight decodeWeight(String detail) {
+        try {
+            return new Weight(Double.parseDouble(detail));
+        } catch (NumberFormatException e) {
+            //Returns an invalid weight for startup to detect
+            return new Weight(Double.MIN_VALUE);
+        }
+    }
+
+    private static Gender decodeGender(String detail) {
+        if (detail.length() > 1) {
+            //Returns an invalid gender for startup to detect
+            return new Gender('X');
+        }
+        return new Gender(detail.charAt(0));
+    }
+
+    private static Age decodeAge(String detail) {
+        try {
+            return new Age(Integer.parseInt(detail));
+        } catch (NumberFormatException e) {
+            //Returns an invalid Age for startup to detect
+            return new Age(Integer.MIN_VALUE);
+        }
+    }
+
+    private static CalorieGoal decodeCalorieGoal(String detail) {
+        try {
+            return new CalorieGoal(Integer.parseInt(detail));
+        } catch (NumberFormatException e) {
+            //Returns an invalid CalorieGoal for startup to detect
+            return new CalorieGoal(Integer.MIN_VALUE);
+        }
+    }
+
+    private static ActivityFactor decodeActivityFactor(String detail) {
+        try {
+            return new ActivityFactor(Integer.parseInt(detail));
+        } catch (NumberFormatException e) {
+            //Returns an invalid ActivityFactor for startup to detect
+            return new ActivityFactor(Integer.MIN_VALUE);
+        }
+    }
 }
